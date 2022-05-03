95727d4512ebe1f87233e00aa4f3d11b85a407c7
FloWambui <gflorencewambui@gmail.com>
2022-05-03 17:03:07 +0300

    update the config file

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL='https://newsapi.org/v2/top-headlines?country=us&apiKey=f4cbfecd4e41465c841393d6b9a1f3c9'
+    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+    SECRET_KEY=os.environ.get('SECRET_KEY')
 
 
 
@@ -25,3 +29,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig
+}
