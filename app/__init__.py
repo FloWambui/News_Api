92c985983e8300230fd4ab2c763f9058f65acb18
FloWambui <gflorencewambui@gmail.com>
2022-05-03 17:16:53 +0300

    update the __init__.py files

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
-from ..config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
 
-bootstrap=Bootstrap(app)
+    app=Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-from app.main import views
-from app.main import error
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
