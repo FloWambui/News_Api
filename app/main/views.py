b41636425347d1a6cae324dc479538e78bcb77b6
FloWambui <gflorencewambui@gmail.com>
2022-05-03 20:14:16 +0300

    add the flask script

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from app import main
+from . import main
 from ..request import get_news
 
 @main.route('/')
---
+++ stash.sh
@@ -0,0 +1,4 @@
+export NEWS_API_KEY='f4cbfecd4e41465c841393d6b9a1f3c9'
+export SECRET_KEY=
+
+python3 manage.py server
