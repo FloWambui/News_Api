e21200353d7d0e873ba563e8d39f08742acb1dbd
FloWambui <gflorencewambui@gmail.com>
2022-05-03 12:04:31 +0300

    add the test instance for the news class

---
+++ app/models/news.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News class to define news objects
+    '''
+
+    def __init__(self, urlToImage,title,description,url,publishedAt ):
+        self.urlToImage=urlToImage
+        self.title=title
+        self.description=description
+        self.url=url
+        self.publishedAt=publishedAt
---
+++ app/news_test.py
@@ -0,0 +1,22 @@
+import unittest
+from models import news
+News=news.News
+
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = news(111245, 'POLITICAL NEWS', 'US PRESIDENCY', '/khsjha27hbs', '01/10/2022')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+
+if __name__ == '__main__':
+    unittest.main()
